d1395b3048f2d4c5cd9c61576a5644f6fa3d0e08
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-13 22:52:12 -0300

    adicionado a camada de service

---
+++ app/Services/CharacterService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\CharacterRepositoryInterface;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+
+class CharacterService
+{
+    public CharacterRepositoryInterface $characterRepository;
+
+    public function __construct(CharacterRepositoryInterface $characterRepository) {
+        $this->characterRepository = $characterRepository;
+    }
+
+    public function getAllCharacters(): Collection
+    {
+        $characters = $this->characterRepository->getAll();
+        $messageError = "Nenhum registro foi encontrado.";
+        return $this->executeFindInTableWithRelations($characters, $messageError);
+    }
+
+    public function findCharactersById(int $id, string $table): Collection
+    {
+        $characters = $this->characterRepository->getByIdWithRelations($id, $table);
+        $tableFirstLetterUpperCase = ucfirst($table);
+        $messageError = "Nenhum registro foi encontrado na tabela {$tableFirstLetterUpperCase}.";
+        return $this->executeFindInTableWithRelations($characters, $messageError);
+    }
+
+    private function executeFindInTableWithRelations(Collection $characters, string $messageError = ''): Collection
+    {
+        if (count($characters) <= 0) {
+            if (!empty($messageError)) {
+                throw new Exception($messageError);
+            }
+        }
+        return $characters;
+    }
+}
